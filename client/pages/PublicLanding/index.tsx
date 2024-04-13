c1980d6afdb9a15296c87cf00a0fcdccca9c0571
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-04-13 14:31:07 -0300

    feat: initial commit

---
+++ client/pages/PublicLanding/index.tsx
@@ -0,0 +1,10 @@
+export const PageLanding = () => {
+  return (
+    <div>
+      <input placeholder="Your CV" />
+      <input placeholder="Job Description" />
+      <input placeholder="API Key" type="password" />
+      <button>Generate Cover Letter</button>
+    </div>
+  );
+};
